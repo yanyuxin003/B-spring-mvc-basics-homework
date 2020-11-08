4e60dc2f7adf5a3345de5419f2c3e1b1080a9a3c
yanyuxin003 <yanyuxin003@163.com>
2020-11-08 11:50:44 +0800

    [yyx]finish register user and throw the exception

---
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/service/UserService.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.capacity.gtb.mvc.service;
+
+import com.thoughtworks.capacity.gtb.mvc.domain.User;
+import com.thoughtworks.capacity.gtb.mvc.exception.UserIsExistException;
+import com.thoughtworks.capacity.gtb.mvc.exception.UserNameAndPasswordIsNullException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class UserService {
+    private Map<Integer, User> userMap = new HashMap<>();
+
+    public UserService() {
+        userMap.put(1, new User(1, "yanyuxin", "123456", "yyx@qq.com"));
+        userMap.put(2, new User(2, "ytj", "22222", "ytj@qq.com"));
+    }
+
+    public void registerUser(User user) {
+        if((user.getUsername()==null)){
+            throw new UserNameAndPasswordIsNullException("用户名不为空");
+        }else if(user.getPassword()==null) {
+            throw new UserNameAndPasswordIsNullException("用户名不为空");
+        }
+        for(int i=0 ;i< userMap.size();i++){
+            if(userMap.get(i).getUsername()==user.getUsername()) {
+                throw new UserIsExistException("用户已存在");
+            }
+        }
+        userMap.put(user.getId(), user);
+    }
+
+}
